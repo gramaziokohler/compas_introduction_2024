053a83158ce79b6a3be8dd1a71f1408b5bf743ac
Gonzalo Casas <casas@arch.ethz.ch>
2024-10-16 10:54:07 +0200

    Conflicting change here!

--- examples/example_script.py
+++ examples/example_script.py
@@ -3,5 +3,5 @@ from random_example_lib import say_hi
 say_hi()
 
 print("Bye from the example_script module!")
-print("Done")
+print("Really not done yet!")
 print("This is a change done by a different person!")
